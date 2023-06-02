8acdd311b63127a313e8108e167bedf5a40fbba1
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-03 01:46:20 +0300

    commit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const getWeatherBtn = document.getElementById("weather-btn");
 const city = document.querySelector(".city-name");
+const feel = document.querySelector(".feels-like");
+const temp = document.querySelector(".temp");
+const max_temp = document.querySelector(".max-temp");
+const min_temp = document.querySelector(".min-temp");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const visibility = document.querySelector(".visibility");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherCard = document.querySelector(".card-display");
 //
 const apiKey = "837d7709853d7ea4af7694c9667e89e9";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
@@ -12,16 +21,27 @@ const getWeather = async (city) => {
   console.log(data);
 
   //
-  if (typeof data.name === "undefined") {
-    document.querySelector(".city-name").innerHTML = "helloooooo";
-    document.querySelector(".feels-like").innerHTML = "";
-    document.querySelector(".max-temp").innerHTML = "";
-    document.querySelector(".min-temp").innerHTML = "";
-  } else {
-    document.querySelector(".city-name").innerHTML = data.name;
-    document.querySelector(".feels-like").innerHTML = data.main.feels_like;
-    document.querySelector(".max-temp").innerHTML = data.main.temp_max;
-    document.querySelector(".min-temp").innerHTML = data.main.temp_min;
+  city.innerHTML = data.name;
+  temp.innerHTML = `${data.main.temp} &degC`;
+  feel.innerHTML = data.main.feels_like;
+  max_temp.innerHTML = data.main.temp_max;
+  min_temp.innerHTML = data.main.temp_min;
+  humidity.innerHTML = data.main.humidity;
+  wind.innerHTML = data.wind.speed;
+  visibility.innerHTML = data.visibility;
+  weatherCard.style.display = "block";
+
+  //
+  if (data.weather[0].main === "Clear") {
+    weatherIcon.src = "./img/icons/clear.png";
+  } else if (data.weather[0].main === "Clouds") {
+    weatherIcon.src = "./img/icons/Clouds.png";
+  } else if (data.weather[0].main === "Rain") {
+    weatherIcon.src = "./img/icons/Rain.png";
+  } else if (data.weather[0].main === "Mist") {
+    weatherIcon.src = "./img/icons/Mist.png";
+  } else if (data.weather[0].main === "Drizzle") {
+    weatherIcon.src = "./img/icons/Drizzle.png";
   }
 };
 
