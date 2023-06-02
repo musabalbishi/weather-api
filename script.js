38ed3b7755585f650668fcfc219aa6f3917f8a0b
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-02 15:16:43 +0300

    api works fine

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const getWeatherBtn = document.getElementById("weather-btn");
 
 //
 const apiKey = "837d7709853d7ea4af7694c9667e89e9";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=mecca";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
-const getWeather = async () => {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const search = document.getElementById("search-city");
+
+const getWeather = async (city) => {
+  const response = await fetch(apiUrl + `&q=${city}` + `&appid=${apiKey}`);
   let data = await response.json();
   console.log(data);
+
+  //
+  document.querySelector(".city-name").innerHTML = data.name;
+  document.querySelector(".feels-like").innerHTML = data.main.feels_like;
 };
 
-getWeatherBtn.addEventListener("click", getWeather);
+getWeatherBtn.addEventListener("click", () => {
+  getWeather(search.value);
+});
