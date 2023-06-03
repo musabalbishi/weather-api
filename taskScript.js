b807a95aebe7f13bd0eb4b1619b1905df2d7ed99
Mosab albishi <90303281+musabalbishi@users.noreply.github.com>
2023-06-03 23:40:50 +0300

    commit

--- taskScript.js
+++ taskScript.js
@@ -1,36 +1,125 @@
-const drags = document.querySelectorAll(".task");
-const drops = document.querySelectorAll(".swim-lane");
+// const todos = document.querySelectorAll(".todo");
+// const all_status = document.querySelectorAll(".status");
+// let draggableTodo = null;
 
-drops.forEach((zone) => {
-  zone.addEventListener("dragover", (e) => {
-    let selected = e.target;
-    e.preventDefault();
+// todos.forEach((todo) => {
+//   todo.addEventListener("dragstart", dragStart);
+//   todo.addEventListener("dragend", dragEnd);
+// });
 
-    const bottomTask = insertAboveTask(zone, e.clientY);
-    const currentTask = selected;
+// function dragStart() {
+//   draggableTodo = this;
+//   setTimeout(() => {
+//     this.style.display = "none";
+//   }, 0);
+//   console.log("dragStart");
+// }
 
-    if (!bottomTask) {
-      zone.appendChild(currentTask);
-    } else {
-      zone.insertBefore(currentTask, bottomTask);
-    }
-  });
-});
+// function dragEnd() {
+//   draggableTodo = null;
+//   setTimeout(() => {
+//     this.style.display = "block";
+//   }, 0);
+//   console.log("dragEnd");
+// }
 
-const insertAboveTask = (zone, mouseY) => {
-  const els = zone.querySelectorAll(".task");
+// all_status.forEach((status) => {
+//   status.addEventListener("dragover", dragOver);
+//   status.addEventListener("dragenter", dragEnter);
+//   status.addEventListener("dragleave", dragLeave);
+//   status.addEventListener("drop", dragDrop);
+// });
 
-  let closestTask = null;
-  let closestOffset = Number.NEGATIVE_INFINITY;
+// function dragOver(e) {
+//   e.preventDefault();
+//   //   console.log("dragOver");
+// }
 
-  els.forEach((task) => {
-    const { top } = task.getBoubdingClientRect();
+// function dragEnter() {
+//   this.style.border = "1px dashed #ccc";
+//   console.log("dragEnter");
+// }
 
-    const offset = mouseY - top;
-    if (offset < 0 && offset > closestOffset) {
-      closestOffset = offset;
-      closestTask = task;
-    }
-  });
-  return closestTask;
-};
+// function dragLeave() {
+//   this.style.border = "none";
+//   console.log("dragLeave");
+// }
+
+// function dragDrop() {
+//   this.style.border = "none";
+//   this.appendChild(draggableTodo);
+//   console.log("dropped");
+// }
+
+// /* modal */
+// const btns = document.querySelectorAll("[data-target-modal]");
+// const close_modals = document.querySelectorAll(".close-modal");
+// const overlay = document.getElementById("overlay");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     document.querySelector(btn.dataset.targetModal).classList.add("active");
+//     overlay.classList.add("active");
+//   });
+// });
+
+// close_modals.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     const modal = btn.closest(".modal");
+//     modal.classList.remove("active");
+//     overlay.classList.remove("active");
+//   });
+// });
+
+// window.onclick = (event) => {
+//   if (event.target == overlay) {
+//     const modals = document.querySelectorAll(".modal");
+//     modals.forEach((modal) => modal.classList.remove("active"));
+//     overlay.classList.remove("active");
+//   }
+// };
+
+// /* create todo  */
+// const todo_submit = document.getElementById("todo_submit");
+
+// todo_submit.addEventListener("click", createTodo);
+
+// function createTodo() {
+//   const todo_div = document.createElement("div");
+//   const input_val = document.getElementById("todo_input").value;
+//   const txt = document.createTextNode(input_val);
+
+//   todo_div.appendChild(txt);
+//   todo_div.classList.add("todo");
+//   todo_div.setAttribute("draggable", "true");
+
+//   /* create span */
+//   const span = document.createElement("span");
+//   const span_txt = document.createTextNode("\u00D7");
+//   span.classList.add("close");
+//   span.appendChild(span_txt);
+
+//   todo_div.appendChild(span);
+
+//   no_status.appendChild(todo_div);
+
+//   span.addEventListener("click", () => {
+//     span.parentElement.style.display = "none";
+//   });
+//   //   console.log(todo_div);
+
+//   todo_div.addEventListener("dragstart", dragStart);
+//   todo_div.addEventListener("dragend", dragEnd);
+
+//   document.getElementById("todo_input").value = "";
+//   todo_form.classList.remove("active");
+//   overlay.classList.remove("active");
+// }
+
+// const close_btns = document.querySelectorAll(".close");
+
+// close_btns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     btn.parentElement.style.display = "none";
+//   });
+// });
